f8a38a36bc02a792a3764bfe0f6d5c633ade774a
s333wi <pandoloraw@gmail.com>
2022-09-02 15:24:25 +0200

    First algorithm solve

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let choiceArray = ["Rock","Paper", "Scissors"];
-let computerScore=0, playerScore=0;
+let choiceArray = ["Rock", "Paper", "Scissors"];
+let computerScore = 0, playerScore = 0;
 function getComputerChoice() {
     return Math.floor(Math.random() * choiceArray.length);
 }
@@ -9,27 +9,33 @@ let computerSelection;
 
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection, computerSelection);
-    
-    if(playerSelection === computerSelection) {
+
+    if (playerSelection === computerSelection) {
         console.log("It's a draw");
-    } else if((playerSelection+1) % 3 === computerSelection){
-        console.log(`${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}`);
+    } else if ((playerSelection + 1) % 3 === computerSelection) {
+        console.log(`You have lost!${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}`);
         computerScore++;
     } else {
-        console.log(`${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}`);
+        console.log(`Yay you have won!!!${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}`);
         playerScore++;
     }
 
 }
 
 function game() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 5; i++) {
         playerSelection = prompt("Choose your option: Rock(0), Paper(1), Scissors(2) ");
-        while (playerSelection<0 || playerSelection<2) {
+        while (playerSelection < 0 || playerSelection > 2 || playerSelection == undefined) {
             playerSelection = prompt("Choose your option: Rock(0), Paper(1), Scissors(2) ");
         }
         computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (computerScore>playerScore) {
+        alert("You've lost, the score was PC: "+computerScore+" - User: "+playerScore);
+    } else {
+        alert("You've won, the score was PC: " +computerScore+" - User: "+playerScore);
     }
 }
 
